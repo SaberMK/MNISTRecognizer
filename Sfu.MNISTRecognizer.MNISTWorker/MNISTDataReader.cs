671748a262cb1ee67cfbaa7e91993d4cc13d55d0
SaberMK <pnegebodyguard@gmail.com>
2019-11-05 19:56:52 +0300

    Initial commit

---
+++ Sfu.MNISTRecognizer.MNISTWorker/MNISTDataReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfu.MNISTRecognizer.MNISTWorker
+{
+    public class MNISTDataReader
+    {
+        private FileStream _mnistStream;
+        
+        public void Load(string filepath)
+        {
+            if (!File.Exists(filepath))
+                throw new FileNotFoundException($"File {filepath} not found!");
+
+            _mnistStream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+        }
+
+        
+
+    }
+}
